59cc8cd1c943dd712c86ed4019a2ca042c6b7897
PaulxA09 <pauloarmey18@gmail.com>
2025-05-25 18:41:18 -0500

    Agregar archivo de configuración de base de datos dbconfig.js

---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+// dbconfig.js
+module.exports = {
+  user: 'sa',
+  password: 'P@u102018.180905',
+  server: 'localhost',
+  database: 'ControlAsistencia_DB',
+  options: {
+    trustServerCertificate: true,
+  }
+};
